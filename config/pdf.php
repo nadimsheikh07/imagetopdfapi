e8e4519e411d033d7085ef10a277002a3e7eb4d3
nadim <nadim.sheikh.07@gmail.com>
2021-03-17 16:34:22 +0530

    update

---
+++ config/pdf.php
@@ -0,0 +1,12 @@
+<?php
+
+
+return [
+    'format'           => 'A4',
+    'author'           => 'John Doe',
+    'subject'          => 'This Document will explain the whole universe.',
+    'keywords'         => 'PDF, Laravel, Package, Peace',
+    'creator'          => 'Laravel Pdf',
+    'display_mode'     => 'fullpage',
+    'tempDir' => public_path('temp')
+];
