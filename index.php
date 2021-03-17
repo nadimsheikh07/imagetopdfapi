5a0999a3d083bac0aa1068aea7fd5f7ede960ab9
nadim <nadim.sheikh.07@gmail.com>
2021-03-17 15:40:57 +0530

    update

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if (file_exists(__DIR__ . '/../storage/framework/maintenance.php')) {
 |
 */
 
-require __DIR__ . '/../imagetopdfapi/vendor/autoload.php';
+require __DIR__ . '/../../../imagetopdfapi/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ require __DIR__ . '/../imagetopdfapi/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__ . '/../imagetopdfapi/bootstrap/app.php';
+$app = require_once __DIR__ . '/../../../imagetopdfapi/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
